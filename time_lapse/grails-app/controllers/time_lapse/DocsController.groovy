2635584d2f23c9ab8ad533458d1b0eaa21993dd3
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-26 10:32:41 -0400

    Fixing support for asciidoctor documentation.

--- time_lapse/grails-app/controllers/time_lapse/DocsController.groovy
+++ time_lapse/grails-app/controllers/time_lapse/DocsController.groovy
@@ -3,22 +3,33 @@ package time_lapse
 
 class DocsController {
 
+
 	def index() { 
 		// attempt to find the adoc file
-		def adocFile = new File("../docs/tlv.adoc")
-		if (!adocFile.exists()) { adocFile = getClass().getResource("/tlv.adoc")?.file }
+		def adocFile = new File("../docs/tlv.adoc") // if running from "grails run-app"
+		if (!adocFile.exists()) { 
+			def adocFileUrl = getClass().getResource("/tlv.adoc") // if running as stand alone artifact
+			if (adocFileUrl) { 	
+				adocFile = new File("tlv.adoc")
+				adocFile.write(adocFileUrl.getText()) }
+		}
 
-		if (adocFile?.exists()) {
-			// attempt to find the html file
-			def htmlFile = new File("../docs/tlv.html")
-			if (!htmlFile.exists()) {  htmlFile = getClass().getResource("/tlv.html")?.file }
+		// attempt to find the html file
+		def htmlFile = new File("../docs/tlv.html") // if running from "grails run-app"
+		if (!htmlFile.exists()) {
+			def htmlFileUrl = getClass().getResource("/tlv.html") // if running as a stand alone artifact
+			if (htmlFileUrl) { 
+				htmlFile = new File("tlv.html")
+				htmlFile.write(htmlFileUrl.getText()) }
+		}
 
+
+		if (adocFile.exists()) {
 			// create the corresponding html if it doesn't exist
-			if (!htmlFile?.exists()) { 
+			if (!htmlFile.exists()) { 
 				def command = "asciidoctor ${adocFile.absolutePath}"
 				command.execute().waitFor()
 				htmlFile = new File("${adocFile.absolutePath.replaceAll(".adoc", ".html")}")
-				println htmlFile
 			}
 
 			if (htmlFile.exists()) {
@@ -26,6 +37,10 @@ class DocsController {
 				render htmlFile.getText()
 			}
 			else { render "Oh no! There was a problem while generating the docs." }
+		}
+		else if (htmlFile.exists()) {
+			response.contentType = "text/html"
+			render htmlFile.getText()
 		}
 		else { render "There doesn't appear to be any docs!" }
 	}
