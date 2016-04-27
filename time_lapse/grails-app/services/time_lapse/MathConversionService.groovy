048abf468f0c7905b7e6ea34f6564e6614510600
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-26 20:09:33 -0400

    Common math conversion service.

---
+++ time_lapse/grails-app/services/time_lapse/MathConversionService.groovy
@@ -0,0 +1,20 @@
+package time_lapse
+
+
+import grails.transaction.Transactional
+
+
+@Transactional
+class MathConversionService {
+
+
+	def convertRadiusToDeltaDegrees(params) {
+		def radius = params.radius as Integer
+
+		/* #m * 1Nm / 1852m * 1min / 1Nm * 1deg / 1min */
+		def deltaDegrees = radius / 1852 / 60
+
+
+		return deltaDegrees
+	}
+}
