86f5a79a5645ed8c6dcdde60043f5a2a1ae347e4
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-05-04 16:08:20 -0400

    Renamed files to incorporate their version number in the filename.

--- time_lapse/grails-app/assets/javascripts/index-bundle.js
+++ time_lapse/grails-app/assets/javascripts/index-bundle.js
@@ -1,9 +1,9 @@
 //= require jquery-2.2.0.min.js
-//= require bootstrap
-//= require moment.js
-//= require bootstrap-datetimepicker
+//= require bootstrap-3.3.6
+//= require moment-2.10.6.js
+//= require bootstrap-datetimepicker-4.15.35
 // //= require bootstrap-slider
-//= require ol
+//= require ol-3.15.1
 //= require spin
 //= require coordinate-conversion
 
