8d80fb5fef25e3e7e484808e5c364de25c8a477f
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-05-11 11:03:55 -0400

    Added a convenience method to help generate filenames from the current date.

--- time_lapse/grails-app/assets/javascripts/prototype.js
+++ time_lapse/grails-app/assets/javascripts/prototype.js
@@ -7,7 +7,7 @@ if (!("contains" in Array.prototype)) {
 		for (var i = 0; i < this.length; i++) {
 			if(this[i] === value) { return true; }
     		}
-    
+
 
 		return false;
 	}
@@ -15,6 +15,22 @@ if (!("contains" in Array.prototype)) {
 
 if (!("contains" in String.prototype)) {
 	String.prototype.contains = function(string) { return this.search(string) > -1; }
+}
+
+if (!("generateFilename" in Date.prototype)) {
+	Date.prototype.generateFilename = function() {
+		var doubleDigitString = function(number) { return number < 10 ? "0" + number : number.toString(); }
+
+		var year = this.getFullYear();
+		var month = doubleDigitString(this.getMonth() + 1);
+		var day = doubleDigitString(this.getDate());
+		var hour = doubleDigitString(this.getHours());
+		var minute = doubleDigitString(this.getMinutes());
+		var second = doubleDigitString(this.getSeconds());
+
+
+		return year + month + day + hour + minute + second;
+	}
 }
 
 if (!("unique" in Array.prototype)) {
@@ -27,6 +43,6 @@ if (!("unique" in Array.prototype)) {
 		}
 
 
-		return array; 
+		return array;
 	}
 }
