1efc8e13d46721e7558cb765723c57affd62aa99
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-05-26 09:51:04 -0400

    Handled the params.max properly.

--- time_lapse/grails-app/controllers/time_lapse/LogsController.groovy
+++ time_lapse/grails-app/controllers/time_lapse/LogsController.groovy
@@ -5,13 +5,13 @@ class LogsController {
 
 
 	def wmsRequest() {
-		if (params.max.isNumber()) { params.max = params.max > 100 ? 100 : params.max }
+		if (params.max?.isNumber()) { params.max = params.max.toInteger() > 100 ? 100 : params.max }
 		else { params.max = 10 }
 		respond WmsRequest.list(params), model:[wmsRequestCount: WmsRequest.count()]
 	}
 
 	def xyzRequest() {
-		if (params.max.isNumber()) { params.max = params.max > 100 ? 100 : params.max }
+		if (params.max?.isNumber()) { params.max = params.max.toInteger() > 100 ? 100 : params.max }
 		else { params.max = 10 }
                 respond XyzRequest.list(params), model:[xyzRequestCount: XyzRequest.count()]
 	}
